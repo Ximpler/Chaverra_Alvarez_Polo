f4b854473cd095864e3656a90ae27ee2555c8214
Ximpler <apmichelle@uninorte.edu.co>
2023-05-23 12:51:19 -0500

    verificador
    se añade el verificador

---
+++ verificardor.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def comparar_archivos(archivo1, archivo2):
+    with open(archivo1, "r", encoding="ISO-8859-1") as f1, open(
+        archivo2, "r", encoding="ISO-8859-1"
+    ) as f2:
+        contenido1 = f1.read()
+        contenido2 = f2.read()
+        if contenido1 == contenido2:
+            return "ok"
+        else:
+            return "nok"
+
+
+filename = sys.argv[1]
+filename_des = sys.argv[2]
+print(comparar_archivos(filename, filename_des))
